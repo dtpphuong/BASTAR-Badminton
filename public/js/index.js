ce1da899e05b739abc677406bf63ea8d20708373
Phuong <duongthanhphuong7413@gmail.com>
2024-08-02 10:36:29 +0700

    fix bug

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,22 @@ window.addEventListener('scroll', function() {
     }
     lastScrollTop = scrollTop;
 });
+
+//modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
